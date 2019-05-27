ba8ef4efb6cac7ae3d6b74b4043a017b8f5ab2d1
nicolas barbosa <nicolas.barbosa@orange.fr>
2019-05-27 10:07:38 +0200

    chat rdy

---
+++ app.js
@@ -0,0 +1,24 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
+const ent = require('ent')
+
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/Client/index.html');
+});
+
+io.sockets.on('connection', function (socket, pseudo) {
+    socket.on('nouveau_client', function (pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    });
+});
+
+server.listen(8080);
